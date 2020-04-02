fd6def6dc3eef92f46783de42ec35ce38155b7e9
Life EXE <lifeexe.art@gmail.com>
2020-04-03 00:35:26 +0300

    31 lesson - Animations | Packaging

--- Source/ShootThemUp/Private/Components/STUCharacterMovementComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUCharacterMovementComponent.cpp
@@ -1,12 +1,11 @@
 // Shoot Them Up Game, All Rights Reserved.
 
-
-#include "STUCharacterMovementComponent.h"
+#include "Components/STUCharacterMovementComponent.h"
 #include "Player/STUBaseCharacter.h"
 
-float USTUCharacterMovementComponent::GetMaxSpeed() const 
+float USTUCharacterMovementComponent::GetMaxSpeed() const
 {
     const float MaxSpeed = Super::GetMaxSpeed();
     const ASTUBaseCharacter* Player = Cast<ASTUBaseCharacter>(GetPawnOwner());
-    return Player && Player->IsRunning() ? MaxSpeed * RunModifier: MaxSpeed;
+    return Player && Player->IsRunning() ? MaxSpeed * RunModifier : MaxSpeed;
 }
